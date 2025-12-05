463645fda99e614db91ac1233e29cbc985386e34
sjkim <07nevergiveup08@gmail.com>
2025-12-05 13:12:48 +0900

    동적세그먼트에 따른 페이지 데이터 조회

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,5 +1,10 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
 const FilteredNewsPage = ({ params }) => {
@@ -7,18 +12,53 @@ const FilteredNewsPage = ({ params }) => {
   // archive 이후의 모든 세그먼트들을 배열로 가져온다. ex) ['2024', '3']
   const filter = params.filter;
 
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
+  let news;
+
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>선택한 년도의 뉴스가 없습니다.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter");
+  }
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>{<Link href={`/archive/${link}`}>{link}</Link>}</li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return <li key={link}>{<Link href={href}>{link}</Link>}</li>;
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
   // const news = getNewsForYear(filter);
 
