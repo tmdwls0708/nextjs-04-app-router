8857d4b40003d75122c13830065c95a2818fc47f
sjkim <07nevergiveup08@gmail.com>
2025-12-09 18:16:26 +0900

    (csr) 백엔드에서 데이터 가져오도록 수정

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,44 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 const NewsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const res = await fetch("http://localhost:8080/news");
+      if (!res.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+      const news = await res.json();
+      setIsLoading(false);
+      setNews(news);
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newContent;
+  if (news) {
+    newContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newContent}
     </>
   );
 };
